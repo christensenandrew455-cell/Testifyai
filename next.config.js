16ecbe321fd73302e2345ffd428181ad230e30c1
christensenandrew455-cell <christensenandrew455@gmail.com>
2025-10-29 15:52:03 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/ads.txt', // the path visitors hit
+        destination: 'https://srv.adstxtmanager.com/19390/thetestifyai.com', // Ezoic Ads.txt Manager
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
