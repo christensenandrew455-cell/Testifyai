e9cb3f0a432f8b60aa7a341d13f164fcecefd1d9
christensenandrew455-cell <christensenandrew455@gmail.com>
2025-11-25 16:21:12 -0500

    Update page.js

--- app/signup/page.js
+++ app/signup/page.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 
